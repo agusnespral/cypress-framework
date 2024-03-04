17d82f618837c35dc53c706d0ce5d344c9e069ec
Agustin <agusnespral@gmail.com>
2024-03-04 20:59:12 -0300

    se agrega un caso con its each e iterar

---
+++ cypress/e2e/online-shop/desafio-1/itsEachIterar.cy.js
@@ -0,0 +1,19 @@
+Cypress.Commands.add('eliminarProducto', (id) => {
+    cy.request({
+        method: "GET",
+        url: `${Cypress.env().baseUrlAPI}/products?id=${id}`,
+        failsOnStatusCode: false,
+        headers: {
+            Authorization: `Bearer ${Cypress.env().token}`
+        }
+    }).its('body.products.docs').each((product) => {
+        cy.request({
+            method: "DELETE",
+            url: `${Cypress.env().baseUrlAPI}/product/${product._id}`,
+            headers: {
+                Authorization: `Bearer ${Cypress.env().token}`,
+            }
+        });
+    });
+
+});
